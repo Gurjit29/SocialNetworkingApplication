c7240bfd56635e418bef97c179e60a4105ce8ab5
Gurjit Khatkar <gurjitkhatkar@Gurjits-MacBook-Pro.local>
2020-07-18 12:01:52 -0400

    Added dynamic navbar based on if user is loggedIn

--- react-frontend/src/components/NavBar.js
+++ react-frontend/src/components/NavBar.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import {SignOut} from '../authentication/auth';
+import {SignOut, isLoggedIn} from '../authentication/auth';
+import { Component } from 'react';
 
 
-const NavBar = () => {
-    return <nav className="navbar navbar-expand-lg navbar-dark fixed-top bg-dark">
+class NavBar extends Component {
+
+  constructor(){
+    super();
+    this.state=({
+      loggedIn:isLoggedIn()
+    })
+  }
+
+  toggleState = (name) => {
+
+    if(name =="signout"){
+      SignOut();
+    }
+    this.setState({
+      loggedIn:!this.state.loggedIn
+    })
+  }
+
+  render() {
+
+    const {loggedIn}=this.state;
+    return  ( <nav className="navbar navbar-expand-lg navbar-dark fixed-top bg-dark">
 <Link className="navbar-brand" exact to="/" href= " # ">QA Engine App</Link>
 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
   <span className="navbar-toggler-icon"></span>
@@ -13,19 +35,25 @@ const NavBar = () => {
 <div className="collapse navbar-collapse" id="navbarNav">
   <ul className="navbar-nav">
    
-    <li className="nav-item">
-    <Link className="nav-link" href=" # " to="/login">Login</Link>
+   
+   {!loggedIn &&
+   <React.Fragment> <li className="nav-item">
+    <Link className="nav-link" href=" # " to="/login" onClick={() => this.toggleState("login")}>Login</Link>
     </li>
     <li className="nav-item">
-    <Link  className="nav-link" href=" # " to="/register">Register</Link>
+    <Link  className="nav-link" href=" # " to="/register" onClick={() => this.toggleState("register")}>Register</Link>
     </li>
-    <li className="nav-item">
-    <Link  className="nav-link" href=" # " to="/" onClick={()=>SignOut()}>Signout</Link>
-    </li>
+   </React.Fragment> }
+
+   {loggedIn && <li className="nav-item">
+    <Link  className="nav-link" href=" # " to="/" onClick={() => this.toggleState("signout")}>Signout</Link>
+   </li> }
     
   </ul>
 </div>
 </nav>
+    )
+  }
 }
 
 
