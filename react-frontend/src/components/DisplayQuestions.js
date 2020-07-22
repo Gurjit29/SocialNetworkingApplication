d8bb4a861f75cfcebdd65401e55e4c6f594c8e8e
Gurjit Khatkar <gurjitkhatkar@Gurjits-MacBook-Pro.local>
2020-07-21 20:15:11 -0400

    Displaying single question on clicking from list of questions working

--- react-frontend/src/components/DisplayQuestions.js
+++ react-frontend/src/components/DisplayQuestions.js
@@ -29,14 +29,17 @@ class DisplayQuestions extends Component {
 
     }
 
+    
+
     renderQuestions = () => {
         const { questions } = this.state;
 
        
 
-       return <ul className="container" style={{ 'listStyle': 'none' }}>
+       return ( <ul className="container" style={{ 'listStyle': 'none' }}>
                     {questions.map((question, index) => {
-                        return <li className="container" key={index} onClick={this.questionClickListener(question)} style={{ 'border': '2px solid black', 'margin': '10px', 'padding': '10px', 'borderRadius': '8px' }}>
+                        return <li className="container" key={index}  
+                        onClick={this.questionClickListener(question)} style={{ 'border': '2px solid black', 'margin': '10px', 'padding': '10px', 'borderRadius': '8px' }}>
                             <h3>{question.title + "   "}
                                 <button type="button" className="btn btn-dark"><b>{"Posted By: " + question.postedBy.name}</b></button>
                             </h3>
@@ -52,7 +55,7 @@ class DisplayQuestions extends Component {
                         </li>
                     })
                     }
-                </ul> 
+                </ul> )
 
     }
 
@@ -63,27 +66,9 @@ class DisplayQuestions extends Component {
 
         return (
             <React.Fragment>
+            
                 {this.renderQuestions()}
-                 {/* <ul className="container" style={{ 'listStyle': 'none' }}>
-                    {questions.map((question, index) => {
-                        return <li className="container" key={index} onClick={this.questionClickListener(question)} style={{ 'border': '2px solid black', 'margin': '10px', 'padding': '10px', 'borderRadius': '8px' }}>
-
-                            <h3>{question.title + "   "}
-                                <button type="button" className="btn btn-dark"><b>{"Posted By: " + question.postedBy.name}</b></button>
-                            </h3>
-                            <p>{question.body}</p>
-                            <ul className="list-group list-group-horizontal" style={{ 'listStyle': 'none' }}>
-                                {question.hashtags.map((hashtag, i) => {
-
-                                    return <li key={i} style={{ 'marginRight': '5px' }} >
-                                        <button type="button" className="btn btn-info"><b># {hashtag}</b></button>
-                                    </li>
-                                })}
-                            </ul>
-                        </li>
-                    })
-                    }
-                </ul>  */}
+                
 
             </React.Fragment>
         )
